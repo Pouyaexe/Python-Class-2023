76814313b1ae6f40fce20dea5138c0dc4aae3a65
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-04-05 10:22:37 +0330

    Add files via upload

---
+++ 1/main.py
@@ -0,0 +1,71 @@
+if 5 > 2:
+    print("5 is greater than two!")
+    
+
+if 5 > 2:
+    print("Five is greater than two!")
+# Syntax Error 
+
+x = 5 #int 
+y = "Ali" #str
+X = "Reza"
+
+z = 3.5 #float
+
+print(z)
+
+z = 1.2
+
+print(z) 
+
+myvar = "Hey!"
+
+x, y, z= "Orange", "black", "Red"
+print(x,y,z)
+
+t = type(x)
+print(t)
+
+print(type(x))
+
+x = y = z = "Shalgham"
+
+Age_1 = 8
+Age_2 = 10
+Age_3 = 12
+
+Age = [8,10,12]
+print(Age)
+
+x,y,z = Age #Unpack
+
+print(x,y,z)
+
+#########
+
+x = "My"
+y = " name"
+z = " is Reza"
+
+print(x,y,z)
+print(x+y+z)
+
+str_age = "Age: "
+age = 25
+print(str_age + str(age))
+
+
+x = True 
+y = False
+
+x = str(2)
+x = int("8")
+
+y = float("5.3")
+
+
+y = int("Ali")
+
+
+# Create a variable named carname and assign the value Volvo to it.
+# 
