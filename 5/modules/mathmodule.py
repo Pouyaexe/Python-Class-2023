0b8948440d2ed0e74366da59c996d69be3ca1c84
Pouya <pouyahallaj@gmail.com>
2023-04-27 22:33:16 +0330

    modified:   5/Answers.ipynb 	new file:   5/modules/main.py 	new file:   5/modules/mathmodule.py

---
+++ 5/modules/mathmodule.py
@@ -0,0 +1,17 @@
+
+
+def factorial(n:int):
+    """
+    This function calculates the factorial of a given number
+    using a non-recursive method.
+    """
+    result = 1
+    
+    if n==0 or n==1:
+        return 1
+    else:
+        for i in range(1,n+1): # i=1, i=2, i=3 ,,,
+            result = result * i
+            # print("i: ", i)  
+            # print("result: ", result)
+        return result
