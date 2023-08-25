f54921310d12f808487f17d05e84f8838962aab2
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-08-25 12:35:01 +0330

    modified:   21/follow.ipynb 	new file:   24/TODO_part2.ipynb 	new file:   24/todo.py Honarble guest: Boss!

---
+++ 24/todo.py
@@ -0,0 +1,66 @@
+class Task:
+    def __init__(self,name, description, status = False) -> None:
+        self.name = name
+        self.description = description
+        self.status = status
+        
+    def mark_completed(self):
+        self.status = True
+    
+    def update_name(self):
+        self.name = input("Enter the new name: ")
+        self.description = input("Enter the new description: ")
+        
+    def __repr__(self) -> str:
+        if self.status:
+            return self.name + " - "+ self.description + " [(Completed)]"
+        else:
+            return self.name + " - "+ self.description + " [(Not Completed)]"
+        
+tasks = []
+
+
+while True:
+    
+    print("--- To-Do List Manager ---")
+    print("1. Add Task")
+    print("2. Edit Task")
+    print("3. Mark Task as Completed")
+    print("4. View Tasks")
+    print("5. Exit")
+    
+    choice = input("Enter your choice(1-5): ")
+    
+    if choice == "1":
+        name = input("Enter the task name: ")
+        desc = input("Input the description: ")
+        tasks.append(Task(name, desc))
+        print("Task added successfully!")
+    
+    # elif choice == "2":
+    #     index = int(input("Enter the task index to edit: ")) - 1
+    #     new_name = input("Enter the new task name: ")
+    #     new_desc = input("Enter the new task description: ")
+    #     task[index].name = new_name
+    #     task[index].description = new_desc
+    #     print("Task Updated seuccessfully!")
+    
+    elif choice == "2": 
+        index = int(input("Enter the task index to edit: ")) - 1
+        tasks[index].update_name()
+        print("Task Updated seuccessfully!")
+        
+    elif choice == "3":
+        index = int(input("Enter the task index to complete: ")) - 1
+        tasks[index].mark_completed()
+        print(f"Task {index + 1} marked as completed")
+    
+    elif choice == "4":
+        print("To-Do List:")
+        for task in tasks:
+            print(task)
+            
+    elif choice == "5":
+        print("Exiting the To-Do List Manager.")
+        break
+    
